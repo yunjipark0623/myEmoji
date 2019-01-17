941295436c9db39a7531b4ab19cfee533d48f50e
박윤지 <bearji0623@gmail.com>
2019-01-17 23:53:03 +0900

    design

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 var face-img = $('.face-img');
+var eye-img = $('.eye-img');
+var mouth-img = $('.mouth-img');
+var x = $('.remove');
+
+
+// 이미지 클릭했을 때 canvas에 보이는 함수
+
+function showImg(event) {
+  var target = event.target;
+
+}
+
+
+// 이미지 삭제 함수
+
+function remove() {
+
+}
+
+$('.remove').on('click', remove);
