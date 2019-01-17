4d24c2e39ce67621acd4ae83e8edf67fe52db25a
jungsehwan0612 <jungsehwan0612@gmail.com>
2019-01-17 23:44:42 +0900

    카카오톡 로그인 & 메시지 보내기 추가

---
+++ route.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, send_file
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from typing import Any
+import os
+from flask_cors import CORS
+from flask import jsonify
+import json
+import urllib.request
+
+app = Flask(__name__, template_folder='templates')
+CORS(app)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return render_template("test.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv('VCAP_APP_PORT', '10000')))
