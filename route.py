c07668410b3be6d8f44fb860f223c4b183459b7a
jungsehwan <jungsehwan0612@gmail.com>
2019-01-18 16:34:48 +0900

    123123

--- route.py
+++ route.py
@@ -12,7 +12,7 @@ CORS(app)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("test.html")
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
